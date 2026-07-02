f39917f7253cc461f78f640a0fc6ca21c7b5c0da
ndeyefatoundour887-design <ndeyefatoundour887@gmail.com>
2026-07-02 18:35:23 +0100

    commit

--- controllers/question.controller.js
+++ controllers/question.controller.js
@@ -32,3 +32,76 @@ exports.AfficherQuestions = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
+
+exports.getQuestionById = async (req, res) => {
+  try {
+    const question = await Question.findById(req.params.id)
+      .populate("auteur", "nom email");
+
+    if (!question) {
+      return res.status(404).json({
+        message: "Question introuvable",
+      });
+    }
+
+    res.status(200).json(question);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+exports.modifierQuestion = async (req, res) => {
+  try {
+    const { titre, description, tags } = req.body;
+
+    const question = await Question.findById(req.params.id);
+
+    if (!question) {
+      return res.status(404).json({
+        message: "Question introuvable",
+      });
+    }
+
+    if (question.auteur.toString() !== req.user.id) {
+      return res.status(403).json({
+        message: "Vous ne pouvez modifier que vos propres questions.",
+      });
+    }
+
+    question.titre = titre;
+    question.description = description;
+    question.tags = tags;
+
+    await question.save();
+
+    res.status(200).json(question);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+exports.supprimerQuestion = async (req, res) => {
+  try {
+    const question = await Question.findById(req.params.id);
+
+    if (!question) {
+      return res.status(404).json({
+        message: "Question introuvable",
+      });
+    }
+
+    await Question.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({
+      message: "Question supprimée avec succès",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
--- routes/question.route.js
+++ routes/question.route.js
@@ -1,12 +1,22 @@
-const express = require('express');
-const { creerQuestion, AfficherQuestions} = require('../controllers/question.controller');
-const authMiddleware = require('../middleware/user.middleware');
+const express = require("express");
+constauthMiddleware = require("../middleware/user.middleware");
+
+const {
+  creerQuestion,
+  AfficherQuestions,
+  getQuestionById,
+   modifierQuestion,
+  supprimerQuestion,
+} = require("../controllers/question.controller");
+
+const authMiddleware = require("../middleware/user.middleware");
 
 const router = express.Router();
 
-router.post('/', authMiddleware, creerQuestion);
+router.post("/", authMiddleware, creerQuestion);
 router.get("/", AfficherQuestions);
-
-
+router.get("/:id", getQuestionById);
+router.put("/:id", authMiddleware, modifierQuestion);
+router.delete("/:id", authMiddleware, supprimerQuestion);
 
 module.exports = router;
